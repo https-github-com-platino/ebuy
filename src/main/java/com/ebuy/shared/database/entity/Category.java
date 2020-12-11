354aa64a8709c77e872d0d02ca03d1022bdf67de
nishad5089 <nishad5089@gmail.com>
2020-12-11 22:48:31 +0600

    message

--- src/main/java/com/ebuy/shared/database/entity/Category.java
+++ src/main/java/com/ebuy/shared/database/entity/Category.java
@@ -1,9 +1,12 @@
 package com.ebuy.shared.database.entity;
+
+import lombok.EqualsAndHashCode;
+
 /**
  * @created 11/12/2020 - 6:34 PM
  * @project ebuy
  * @author Abdur Rahim Nishad
  */
-
+@EqualsAndHashCode(callSuper = true)
 public class Category extends BaseEntity{
 }
