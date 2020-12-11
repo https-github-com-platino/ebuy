a2f2bb259c3dc469990ced80c226fcb468d786cd
nazfisa <nazimuddinasifiiuc@gmail.com>
2020-12-11 23:05:00 +0600

    update product story

--- src/main/java/com/ebuy/Application.java
+++ src/main/java/com/ebuy/Application.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class EbuyApplication {
+public class Application {
 
     public static void main(String[] args) {
-        SpringApplication.run(EbuyApplication.class, args);
+        SpringApplication.run(Application.class, args);
     }
 
 }
--- src/main/java/com/ebuy/shared/database/entity/Category.java
+++ src/main/java/com/ebuy/shared/database/entity/Category.java
@@ -1,9 +1,22 @@
 package com.ebuy.shared.database.entity;
+
+import lombok.Data;
+
+import javax.persistence.*;
+import java.util.List;
+
 /**
  * @created 11/12/2020 - 6:34 PM
  * @project ebuy
- * @author Abdur Rahim Nishad
+ * @author Nazim Uddin Asif
  */
 
+@Entity
+@Data
 public class Category extends BaseEntity{
+
+    private String name;
+
+    @OneToMany(fetch = FetchType.LAZY,mappedBy = "category", cascade = CascadeType.ALL)
+    private List<Product> product;
 }
