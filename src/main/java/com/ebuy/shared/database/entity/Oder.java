4480f9084e2f882521a08010de91a94dc2c0e152
nazfisa <nazimuddinasifiiuc@gmail.com>
2020-12-11 18:53:50 +0600

    update

--- src/main/java/com/ebuy/shared/database/entity/Oder.java
+++ src/main/java/com/ebuy/shared/database/entity/Oder.java
@@ -3,6 +3,7 @@ package com.ebuy.shared.database.entity;
 /**
  * @created 11/12/2020 - 6:40 PM
  * @project ebuy
+ * @author Nazim Uddin Asif
  * @author Abdur Rahim Nishad
  */
 public class Oder extends BaseEntity{
